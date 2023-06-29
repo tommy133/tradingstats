3e012aa8cb91ea3653e3c685dd128cfe39fb6575
Tomeu <tomestrany@gmail.com>
2023-06-29 18:16:48 +0200

    refactor: start server

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ const server = require("./server");
 
 //PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 8080;
-server.listen(port, () => console.log(`Server running on port ${port}..`));
+const startServer = () => {
+    server.listen(port, () => console.log(`Server running on port ${port}..`));
+}
+
+startServer();
