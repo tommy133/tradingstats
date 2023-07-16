0f4b40424132bde134f46d619ee7a34987142098
Tomeu <tomestrany@gmail.com>
2023-07-16 15:22:27 +0200

    feat: file management

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import projectionRoutes from './routes/projection';
 import symbolRoutes from './routes/symbol';
 import statusRoutes from './routes/status';
 import pcommentRoutes from './routes/pcomment';
+import fileRoutes from './routes/file';
 
 // Configuring express server
 app.use(bodyParser.json());
@@ -20,5 +21,6 @@ app.use('/projections', projectionRoutes);
 app.use('/symbols', symbolRoutes);
 app.use('/statuses', statusRoutes);
 app.use('/pcomment', pcommentRoutes);
+app.use('/file', fileRoutes);
 
 export default app;
