8fe79a9b5b094b4bff59c29efddfea95e91520d0
Tomeu <tomestrany@gmail.com>
2024-04-25 17:48:28 +0200

    remove superuser auth on docker commands

--- tstats_srv_deploy.sh
+++ tstats_srv_deploy.sh
@@ -9,9 +9,9 @@ docker push $DOCKER_IMAGE
 
 #recreate image/container in remote
 ssh -t tomeu@192.168.1.196 "
-    sudo docker stop $DOCKER_CONTAINER_NAME
-    sudo docker rm $DOCKER_CONTAINER_NAME
-    sudo docker rmi $DOCKER_IMAGE
+    docker stop $DOCKER_CONTAINER_NAME
+    docker rm $DOCKER_CONTAINER_NAME
+    docker rmi $DOCKER_IMAGE
 
-    sudo docker pull $DOCKER_IMAGE
-    sudo docker run -d -p 8080:8080 --name $DOCKER_CONTAINER_NAME -e DB_HOST='172.18.0.2' -e DB_NAME='db_tstats' -e DB_USERNAME='mysql_user' -e DB_PASSWORD='mysql_password' --network node-api-network $DOCKER_IMAGE"
+    docker pull $DOCKER_IMAGE
+    docker run -d -p 8080:8080 --name $DOCKER_CONTAINER_NAME -e DB_HOST='172.18.0.2' -e DB_NAME='db_tstats' -e DB_USERNAME='mysql_user' -e DB_PASSWORD='mysql_password' --network node-api-network $DOCKER_IMAGE"
