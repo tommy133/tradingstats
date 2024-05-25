6e7d23f36685e6872545783a9e35902f803f1898
Tomeu <tomestrany@gmail.com>
2024-05-25 13:29:39 +0200

    correct endpoint name comments

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import compression from "compression";
-import projectionRoutes from "./routes/projection";
-import symbolRoutes from "./routes/symbol";
-import statusRoutes from "./routes/status";
-import pcommentRoutes from "./routes/pcomment";
-import operationRoutes from "./routes/operation";
-import opcommentRoutes from "./routes/opcomment";
+import cors from "cors";
+import express from "express";
 import marketRoutes from "./routes/market";
+import opcommentRoutes from "./routes/opcomment";
+import operationRoutes from "./routes/operation";
+import pcommentRoutes from "./routes/pcomment";
+import projectionRoutes from "./routes/projection";
+import statusRoutes from "./routes/status";
+import symbolRoutes from "./routes/symbol";
 
 const app = express();
 app.use(compression());
@@ -22,8 +22,8 @@ app.use("/projections", projectionRoutes);
 app.use("/symbols", symbolRoutes);
 app.use("/statuses", statusRoutes);
 app.use("/markets", marketRoutes);
-app.use("/pcomment", pcommentRoutes);
+app.use("/pcomments", pcommentRoutes);
 app.use("/operations", operationRoutes);
-app.use("/opcomment", opcommentRoutes);
+app.use("/opcomments", opcommentRoutes);
 
 export default app;
