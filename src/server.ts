caf0fee6e768c688cbbd77dde20a16276c139420
Tomeu <tomestrany@gmail.com>
2024-08-05 20:59:44 +0200

    pull replication POC works

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import bodyParser from "body-parser";
 import compression from "compression";
 import cors from "cors";
 import express from "express";
-import { Collection, Document, MongoClient } from "mongodb";
 import marketRoutes from "./routes/market";
 import opcommentRoutes from "./routes/opcomment";
 import operationRoutes from "./routes/operation";
@@ -11,7 +10,8 @@ import projectionRoutes from "./routes/projection";
 import statusRoutes from "./routes/status";
 import symbolRoutes from "./routes/symbol";
 
-import { lastOfArray } from "rxdb/plugins/core";
+import { QueryError } from "mysql2";
+import mysqlConnection from "./config/db";
 
 const app = express();
 app.use(compression());
@@ -29,54 +29,37 @@ app.use("/pcomments", pcommentRoutes);
 app.use("/operations", operationRoutes);
 app.use("/opcomments", opcommentRoutes);
 
-let mongoCollection: Collection<Document>;
-const func = async () => {
-  console.log("Connecting to MongoDB");
-  const mongoClient = new MongoClient("mongodb://localhost:27017/");
-
-  const mongoConnection = await mongoClient.connect();
-  console.log("CONNECTION " + mongoConnection);
-
-  const mongoDatabase = mongoConnection.db("db_tstats");
-  mongoCollection = await mongoDatabase.collection("projections");
-  console.log("Connected to MongoDB");
-};
-
-func().catch((error) => console.error("Error connecting to MongoDB:", error));
-
-app.get("/pull", async (req: any, res: any) => {
-  console.log("Received request to /pull");
+app.get("/pull", (req: any, res: any) => {
   const id = req.query.id;
-  const updatedAt = parseFloat(req.query.updatedAt);
-  try {
-    const documents = await mongoCollection
-      .find({
-        $or: [
-          {
-            updateAt: { $gt: updatedAt },
-          },
-          {
-            updateAt: { $eq: updatedAt },
-            id: { $gt: id },
-          },
-        ],
-      })
-      .limit(parseInt(req.query.batchSize, 10))
-      .toArray();
-    const newCheckpoint =
-      documents.length === 0
-        ? { id, updatedAt }
-        : {
-            id: lastOfArray(documents)?.id,
-            updatedAt: lastOfArray(documents)?.updatedAt,
-          };
-    console.log("Sending response from /pull");
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ documents, checkpoint: newCheckpoint }));
-  } catch (error) {
-    console.error("Error in /pull:", error);
-    res.status(500).send("Internal Server Error");
-  }
+  const updatedAtUnix = parseFloat(req.query.updatedAt);
+  const batchSize = parseInt(req.query.batchSize, 10);
+  const updatedAtParsed = new Date(updatedAtUnix).toISOString();
+  /**
+   * Notice that we have to compare the updatedAt AND the id field
+   * because the updateAt field is not unique and when two documents have
+   * the same updateAt, we can still "sort" them by their id.
+   */
+
+  mysqlConnection.query(
+    `SELECT * FROM projection WHERE (updated_at > ? OR (updated_at = ? AND id_proj > ?)) ORDER BY updated_at, id_proj LIMIT ?`,
+    [updatedAtParsed, updatedAtParsed, id, batchSize],
+    (err: QueryError | null, rows: any[], fields: any) => {
+      if (!err) {
+        const documents = rows;
+        const lastDocument = documents[documents.length - 1];
+        const newCheckpoint =
+          documents.length === 0
+            ? { id, updatedAtParsed }
+            : {
+                id: lastDocument.id,
+                updatedAt: lastDocument.updatedAt,
+              };
+
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ documents, checkpoint: newCheckpoint }));
+      } else console.log(err);
+    }
+  );
 });
 
 export default app;
